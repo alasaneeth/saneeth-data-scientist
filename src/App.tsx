cfd75c5b59b5e535d115a49fa8fce4ba5ffbc3f6
alasaneeth <alasaneeth22@gmail.com>
2025-10-29 06:58:52 +0400

    static files

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import Header from './components/Pages/Header'
+import "./App.css"
+import Hero from './components/Pages/Hero'
+import About from './components/Pages/About'
+import TechincalSkills from './components/Pages/TechincalSkills'
+import SoftSkills from './components/Pages/SoftSkills'
+import Services from './components/Pages/Service'
+import Contact from './components/Pages/Contact'
+import Footer from './components/Pages/Footer'
+import Testimonial from './components/Pages/Testimonials'
+import Certifications from './components/Pages/Certifications'
+
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <Hero/>
+      <About/>
+      {/* <Projects/> */}
+      <TechincalSkills/>
+      <SoftSkills/>
+      <Certifications/>
+      <Services/>
+      <Testimonial/>
+      <Contact/>
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default App
