f087080a29485748e8481e8a6da2f2c42a603b63
alasaneeth <alasaneeth22@gmail.com>
2025-10-29 07:57:10 +0400

    Technical skills

---
+++ src/components/ExportedFiles/TecknicalSkills.tsx
@@ -0,0 +1,24 @@
+import python from "../../assets/images/python.png";
+import R from "../../assets/images/R.jpg";
+import JupyterNotebook from "../../assets/images/jupyterNotebook.png"
+import Matplotlipt from "../../assets/images/matplotlipt.png"
+import Numpy from "../../assets/images/numy.png"
+import Pandas from "../../assets/images/pandas.png" 
+import ScikitLearn from "../../assets/images/scikitLearn.png"
+import SciPy from "../../assets/images/scipy.png"
+import seaborn from "../../assets/images/seaborn.png"
+
+
+export const TECHNICAL_SKILLS = [
+ 
+  { icon: <img src={python} alt="Python" />, label: "Python" },
+  { icon: <img src={R} alt="R" />, label: "R" },
+  { icon: <img src={JupyterNotebook} alt="JupyterNotebook" />, label: "Jupyter Notebook" },
+  { icon: <img src={Matplotlipt} alt="Matplotlip" />, label: "Matplotlip" },
+  { icon: <img src={Numpy} alt="Numpy" />, label: "Numpy" },
+  { icon: <img src={Pandas} alt="Pandas" />, label: "Pandas" },
+  { icon: <img src={ScikitLearn} alt="ScikitLearn" />, label: "ScikitLearn" },
+  { icon: <img src={SciPy} alt="SciPy" />, label: "SciPy" },
+  { icon: <img src={seaborn} alt="seaborn" />, label: "seaborn" },
+
+];
